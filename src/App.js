fbfedec6cc00c786c4d043c2a338290d0e9fcb13
Madison Beatty <madison.beatty@valpo.edu>
2021-05-08 13:57:27 -0500

    sign up form complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import SignUpForm from "../src/components/SignUpForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    hasSignedUp: false,
+  };
+
+  handleSignUp = (e) => {
+    e.preventDefault()
+    this.setState({
+      hasSignedUp: !this.state.hasSignedUp
+    }
+
+    )
+  };
+  render() {
+    return (
+      <div className="App">
+        <SignUpForm onSignUp={this.handleSignUp} />
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+  state ={
+      email: '',
+      password: ''
+  }
+handleChange = (e) => {
+    console.log(e)
+}
+updateEmail = (e) => {
+    this.setState({
+        email: e.target.value
+    })
+}
+updatePassword = (e)=> {
+    this.setState({
+        password: e.target.value
+    })
+}
+    render() {
+    return (
+    <form onSubmit={this.props.onSignUp}>
+        <input type="text" placeholder="Email" value={this.state.email} onChange ={this.updateEmail} />
+        <input type="password" placeholder='Password' value={this.state.email} onChange={this.updatePassword}/>
+        <button type='submit'>Sign Up</button>
+    </form>
+    )
+  }
+}
+
+export default SignUpForm;
